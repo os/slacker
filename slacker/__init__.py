b0bf11b3f88a9942f5e305409dcba6c7d9d399e7
os <oktaysancak@gmail.com>
2014-05-24 19:49:04 +1000

    Various refactoring.

--- slacker/__init__.py
+++ slacker/__init__.py
@@ -17,7 +17,7 @@ import json
 import requests
 
 
-API_BASE_URL = 'https://slack.com/api/{method}?token={token}'
+API_BASE_URL = 'https://slack.com/api/{api}?token={token}'
 
 
 __all__ = ['Slacker', 'Error']
@@ -39,169 +39,176 @@ class API(object):
     def __init__(self, token):
         self.token = token
 
-    def _request(self, method, **kwargs):
-        response = requests.get(API_BASE_URL.format(method=method, token=self.token),
-                                params=kwargs)
+    def _request(self, method, api, **kwargs):
+        response = method(API_BASE_URL.format(api=api, token=self.token),
+                          **kwargs)
         assert response.status_code == 200
-        response = Response(response.text)
 
+        response = Response(response.text)
         if not response.successful:
             raise Error(response.error)
 
         return response
 
+    def get(self, api, **kwargs):
+        return self._request(requests.get, api, **kwargs)
+
+    def post(self, api, **kwargs):
+        return self._request(requests.post, api, **kwargs)
+
 
 class Auth(API):
     def test(self):
-        return self._request('auth.test')
+        return self.get('auth.test')
 
 
 class Users(API):
     def list(self):
-        return self._request('users.list')
+        return self.get('users.list')
 
 
 class Groups(API):
     def list(self, exclude_archived=None):
-        return self._request('groups.list', exclude_archived=exclude_archived)
+        return self.get('groups.list',
+                        params={'exclude_archived': exclude_archived})
 
-    def history(self, channel,
-                latest=None, oldest=None, count=None):
-        return self._request('groups.history',
-                             channel=channel,
-                             latest=latest,
-                             oldest=oldest,
-                             count=count)
+    def history(self, channel, latest=None, oldest=None, count=None):
+        return self.get('groups.history',
+                        params={
+                            'channel': channel,
+                            'latest': latest,
+                            'oldest': oldest,
+                            'count': count
+                        })
 
 
 class Channels(API):
     def list(self, exclude_archived=None):
-        return self._request('channels.list',
-                             exclude_archived=exclude_archived)
+        return self.get('channels.list',
+                        params={'exclude_archived': exclude_archived})
 
-    def history(self, channel,
-                latest=None, oldest=None, count=None):
-        return self._request('channels.history',
-                             channel=channel,
-                             latest=latest,
-                             oldest=oldest,
-                             count=count)
+    def history(self, channel, latest=None, oldest=None, count=None):
+        return self.get('channels.history',
+                        params={
+                            'channel': channel,
+                            'latest': latest,
+                            'oldest': oldest,
+                            'count': count
+                        })
 
     def mark(self, channel, ts):
-        return self._request('channels.mark',
-                             channel=channel,
-                             ts=ts)
+        return self.get('channels.mark',
+                        params={'channel': channel, 'ts': ts})
 
 
 class Chat(API):
-    def post_message(self, channel, text,
-                     username=None, parse=None, link_names=None,
-                     attachments=None, unfurl_links=None, icon_url=None,
-                     icon_emoji=None):
-        return self._request('chat.postMessage',
-                             channel=channel,
-                             text=text,
-                             username=username,
-                             parse=parse,
-                             link_names=link_names,
-                             attachments=attachments,
-                             unfurl_links=unfurl_links,
-                             icon_url=icon_url,
-                             icon_emoji=icon_emoji)
+    def post_message(self, channel, text, username=None, parse=None,
+                     link_names=None, attachments=None, unfurl_links=None,
+                     icon_url=None, icon_emoji=None):
+        return self.get('chat.postMessage',
+                        params={
+                            'channel': channel,
+                            'text': text,
+                            'username': username,
+                            'parse': parse,
+                            'link_names': link_names,
+                            'attachments': attachments,
+                            'unfurl_links': unfurl_links,
+                            'icon_url': icon_url,
+                            'icon_emoji': icon_emoji
+                        })
 
 
 class IM(API):
     def list(self):
-        return self._request('im.list')
+        return self.get('im.list')
 
-    def history(self, channel,
-                latest=None, oldest=None, count=None):
-        return self._request('im.history',
-                             channel=channel,
-                             latest=latest,
-                             oldest=oldest,
-                             count=count)
+    def history(self, channel, latest=None, oldest=None, count=None):
+        return self.get('im.history',
+                        params={
+                            'channel': channel,
+                            'latest': latest,
+                            'oldest': oldest,
+                            'count': count
+                        })
 
 
 class Search(API):
-    def all(self, query,
-            sort=None, sort_dir=None, highlight=None, count=None, page=None):
-        return self._request('search.all',
-                             query=query,
-                             sort=sort,
-                             sort_dir=sort_dir,
-                             highlight=highlight,
-                             count=count,
-                             page=page)
+    def all(self, query, sort=None, sort_dir=None, highlight=None, count=None,
+            page=None):
+        return self.get('search.all',
+                        params={
+                            'query': query,
+                            'sort': sort,
+                            'sort_dir': sort_dir,
+                            'highlight': highlight,
+                            'count': count,
+                            'page': page
+                        })
 
-    def files(self, query,
-              sort=None, sort_dir=None, highlight=None, count=None, page=None):
-        return self._request('search.files',
-                             query=query,
-                             sort=sort,
-                             sort_dir=sort_dir,
-                             highlight=highlight,
-                             count=count,
-                             page=page)
+    def files(self, query, sort=None, sort_dir=None, highlight=None,
+              count=None, page=None):
+        return self.get('search.files',
+                        params={
+                            'query': query,
+                            'sort': sort,
+                            'sort_dir': sort_dir,
+                            'highlight': highlight,
+                            'count': count,
+                            'page': page
+                        })
 
-    def messages(self, query,
-                 sort=None, sort_dir=None, highlight=None, count=None,
-                 page=None):
-        return self._request('search.messages',
-                             query=query,
-                             sort=sort,
-                             sort_dir=sort_dir,
-                             highlight=highlight,
-                             count=count,
-                             page=page)
+    def messages(self, query, sort=None, sort_dir=None, highlight=None,
+                 count=None, page=None):
+        return self.get('search.messages',
+                        params={
+                            'query': query,
+                            'sort': sort,
+                            'sort_dir': sort_dir,
+                            'highlight': highlight,
+                            'count': count,
+                            'page': page
+                        })
 
 
 class Files(API):
-    def list(self, user=None, ts_from=None, ts_to=None, types=None, count=None,
-             page=None):
-        return self._request('files.list',
-                             user=user,
-                             ts_from=ts_from,
-                             ts_to=ts_to,
-                             types=types,
-                             count=count,
-                             page=page)
+    def list(self, user=None, ts_from=None, ts_to=None, types=None,
+             count=None, page=None):
+        return self.get('files.list',
+                        params={
+                            'user': user,
+                            'ts_from': ts_from,
+                            'ts_to': ts_to,
+                            'types': types,
+                            'count': count,
+                            'page': page
+                        })
 
     def info(self, file, count=None, page=None):
-        return self._request('files.info', file=file, count=count, page=page)
+        return self.get('files.info',
+                        params={'file': file, 'count': count, 'page': page})
 
-    def upload(self, file,
-               filetype=None, filename=None, title=None, initial_comment=None,
-               channels=None):
+    def upload(self, file, filetype=None, filename=None, title=None,
+               initial_comment=None, channels=None):
         with open(file, 'rb') as f:
             if isinstance(channels, (tuple, list)):
                 channels = ','.join(channels)
 
-            response = requests.post(API_BASE_URL.format(method='files.upload',
-                                                         token=self.token),
-                                     params={
-                                        'filetype': filetype,
-                                        'filename': filename,
-                                        'title': title,
-                                        'initial_comment': initial_comment,
-                                        'channels': channels
-                                     },
-                                     files={'file': f})
-            assert response.status_code == 200
-            response = Response(response.text)
-
-            if not response.successful:
-                raise Error(response.error)
-
-            return response
+            return self.post('files.upload',
+                             params={
+                                 'filetype': filetype,
+                                 'filename': filename,
+                                 'title': title,
+                                 'initial_comment': initial_comment,
+                                 'channels': channels
+                             },
+                             files={'file': f})
 
 
 class Stars(API):
     def list(self, user=None, count=None, page=None):
-        return self._request('stars.list',
-                             user=user,
-                             count=count,
-                             page=page)
+        return self.get('stars.list',
+                        params={'user': user, 'count': count, 'page': page})
 
 
 class Slacker(object):
