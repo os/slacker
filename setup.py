6d7aeefc14816d3b87714637e21d5df555efef33
os <oktaysancak@gmail.com>
2014-08-07 18:30:41 +1000

    Release v0.1.7.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='slacker',
-      version='0.1.6',
+      version='0.1.7',
       packages=['slacker'],
       description='Slack API client',
       author='Oktay Sancak',
