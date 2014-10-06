53a222faed7585bf3d0e2b8412159b9adeca82d3
os <oktaysancak@gmail.com>
2014-10-06 12:46:15 +1100

    Add users.info API.

--- slacker/__init__.py
+++ slacker/__init__.py
@@ -77,6 +77,9 @@ class Auth(BaseAPI):
 
 
 class Users(BaseAPI):
+    def info(self, user):
+        return self.get('users.info', params={'user': user})
+
     def list(self):
         return self.get('users.list')
 
