89adb954f7110ed5be8911ab434b3a559672f986
os <oktaysancak@gmail.com>
2014-10-18 15:49:14 +1100

    Add setup classifiers. Fixes #11.

--- setup.py
+++ setup.py
@@ -10,4 +10,15 @@ setup(name='slacker',
       url='http://github.com/os/slacker/',
       install_requires=['requests >= 2.2.1'],
       license='http://www.apache.org/licenses/LICENSE-2.0',
+      classifiers=(
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4'
+      ),
       keywords='slack api')
