6246f1393af742ea5bce689912de3a8659475590
Oktay Sancak <oktaysancak@gmail.com>
2015-06-27 21:02:46 +1000

    Update license.

--- slacker/__init__.py
+++ slacker/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Oktay Sancak
+# Copyright 2015 Oktay Sancak
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
