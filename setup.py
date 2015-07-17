364bbe861c5544454a5a7e4e89a50a67c3c79ca2
Oktay Sancak <oktaysancak@gmail.com>
2015-07-17 23:46:12 +1000

    Set version number to 0.6.8.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='slacker',
-    version='0.6.3',
+    version='0.6.8',
     packages=['slacker'],
     description='Slack API client',
     author='Oktay Sancak',
