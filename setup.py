71558d362fc40f2525c63ae4a472b345db2a5d5d
Oktay Sancak <oktaysancak@gmail.com>
2018-12-08 22:22:37 +1100

    Set version number to 0.12.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
     name='slacker',
-    version='0.11.0',
+    version='0.12.0',
     packages=['slacker'],
     description='Slack API client',
     long_description=readme,
