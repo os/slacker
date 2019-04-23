15d902bac11fa8842a3482401f5adcfa40a48c60
Oktay Sancak <oktaysancak@gmail.com>
2019-04-23 19:30:09 +1000

    Remove blank line

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,7 +6,6 @@ from slacker.utils import get_item_id_by_name
 class TestGetItemIDByName(unittest.TestCase):
     def test_get_item_id_by_name(self):
         list_dict = [{'name': 'channel_name', 'id': '123'}, {}]
-
         self.assertEqual(
             '123', get_item_id_by_name(list_dict, 'channel_name')
         )
