b5332e11f2124076d6d2faef7770ef39836aa9df
Oktay Sancak <oktaysancak@gmail.com>
2019-04-24 00:21:26 +1000

    Add `get_api_url` helper function

--- slacker/utilities.py
+++ slacker/utilities.py
@@ -1,5 +1,14 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+def get_api_url(method):
+    """
+    Returns API URL for the given method.
+
+    :param method: Method name
+    :type method: str
+
+    :returns: API URL for the given method
+    :rtype: str
+    """
+    return 'https://slack.com/api/{}'.format(method)
 
 
 def get_item_id_by_name(list_dict, key_name):
